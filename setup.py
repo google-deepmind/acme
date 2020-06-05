9ce1a2ab00403d8b9f5d395f5887f692d25a5810
John Aslanides <jaslanides@google.com>
2020-06-05 08:29:36 -0700

    Disable 3.8 tests for now.
    PiperOrigin-RevId: 314932259
    Change-Id: Iba7095bffbba67035831f9ea996ef678e5a107af

--- setup.py
+++ setup.py
@@ -95,7 +95,6 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
