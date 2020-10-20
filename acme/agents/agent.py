d90e9b6b19da1c8ab5528f1be01a334c8a5d9849
Acme Contributor <noreply@google.com>
2020-10-20 08:02:03 -0700

    Minor readability improvements to Acme Agent.
    PiperOrigin-RevId: 338060147
    Change-Id: Ifa42de9d6a03d400966d88a5925eed8e4cabfc2f

--- acme/agents/agent.py
+++ acme/agents/agent.py
@@ -25,6 +25,22 @@ import dm_env
 import numpy as np
 
 
+def _calculate_num_learner_steps(num_observations: int,
+                                 min_observations: int,
+                                 observations_per_step: float) -> int:
+  """Calculates the number of learner steps to do at step=num_observations."""
+  n = num_observations - min_observations
+  if n < 0:
+    # Do not do any learner steps until you have seen min_observations.
+    return 0
+  if observations_per_step > 1:
+    # One batch every 1/obs_per_step observations, otherwise zero.
+    return int(n % int(observations_per_step) == 0)
+  else:
+    # Always return 1/obs_per_step batches every observation.
+    return int(1 / observations_per_step)
+
+
 class Agent(core.Actor, core.VariableSource):
   """Agent class which combines acting and learning.
 
@@ -36,30 +52,19 @@ class Agent(core.Actor, core.VariableSource):
   The only real logic implemented by this class is that it controls the number
   of observations to make before running a learner step. This is done by
   passing the number of `min_observations` to use and a ratio of
-  `observations_per_step`
+  `observations_per_step` := num_actor_actions / num_learner_steps.
 
-  Note that the number of `observations_per_step` which can also be in the range
-  [0, 1] in order to allow more steps per update.
+  Note that the number of `observations_per_step` can also be in the range[0, 1]
+  in order to allow the agent to take more than 1 learner step per action.
   """
 
   def __init__(self, actor: core.Actor, learner: core.Learner,
                min_observations: int, observations_per_step: float):
     self._actor = actor
     self._learner = learner
-
-    # We'll ignore the first min_observations when determining whether to take
-    # a step and we'll do so by making sure num_observations >= 0.
-    self._num_observations = -min_observations
-
-    # Rather than work directly with the observations_per_step ratio we can
-    # figure out how many observations or steps to run per update, one of which
-    # should be one.
-    if observations_per_step >= 1.0:
-      self._observations_per_update = int(observations_per_step)
-      self._steps_per_update = 1
-    else:
-      self._observations_per_update = 1
-      self._steps_per_update = int(1.0 / observations_per_step)
+    self._min_observations = min_observations
+    self._observations_per_step = observations_per_step
+    self._num_observations = 0
 
   def select_action(self, observation: types.NestedArray) -> types.NestedArray:
     return self._actor.select_action(observation)
@@ -67,25 +72,21 @@ class Agent(core.Actor, core.VariableSource):
   def observe_first(self, timestep: dm_env.TimeStep):
     self._actor.observe_first(timestep)
 
-  def observe(
-      self,
-      action: types.NestedArray,
-      next_timestep: dm_env.TimeStep,
-  ):
+  def observe(self, action: types.NestedArray, next_timestep: dm_env.TimeStep):
     self._num_observations += 1
     self._actor.observe(action, next_timestep)
 
   def update(self):
-    # Only allow updates after some minimum number of observations have been and
-    # then at some period given by observations_per_update.
-    if (self._num_observations >= 0 and
-        self._num_observations % self._observations_per_update == 0):
-      self._num_observations = 0
-
-      # Run a number of learner steps (usually gradient steps).
-      for _ in range(self._steps_per_update):
-        self._learner.step()
-      # Update actor weights after learner, note in TF this may be a no-op.
+    num_steps = _calculate_num_learner_steps(
+        num_observations=self._num_observations,
+        min_observations=self._min_observations,
+        observations_per_step=self._observations_per_step,
+    )
+    for _ in range(num_steps):
+      # Run learner steps (usually means gradient steps).
+      self._learner.step()
+    if num_steps > 0:
+      # Update the actor weights when learner updates.
       self._actor.update()
 
   def get_variables(self, names: List[str]) -> List[List[np.ndarray]]:
