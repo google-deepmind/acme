e239c14a5708bd2904c356458691618f7f4ef936
Danila Sinopalnikov <sinopalnikov@google.com>
2020-10-27 11:09:01 -0700

    Distributional head for MultivariateNormalDiag distribution.
    PiperOrigin-RevId: 339287601
    Change-Id: I8c7588a7cbccfabb889893c9871f3b03667e2947

--- acme/jax/networks/distributional.py
+++ acme/jax/networks/distributional.py
@@ -18,6 +18,8 @@
 from typing import Optional
 
 import haiku as hk
+import jax
+import jax.numpy as jnp
 import tensorflow_probability
 tfd = tensorflow_probability.experimental.substrates.jax.distributions
 
@@ -33,6 +35,28 @@ class CategoricalHead(hk.Module):
     super().__init__(name=name)
     self._linear = hk.Linear(num_values)
 
-  def __call__(self, inputs, *args, **kwargs):
+  def __call__(self, inputs: jnp.ndarray) -> tfd.Distribution:
     logits = self._linear(inputs)
     return tfd.Categorical(logits=logits)
+
+
+class MultivariateNormalDiagHead(hk.Module):
+  """Module that produces a tfd.MultivariateNormalDiag distribution."""
+
+  def __init__(self, num_dimensions: int, min_scale: float = 1e-6):
+    """Initialization.
+
+    Args:
+      num_dimensions: Number of dimensions of MVN distribution.
+      min_scale: Minimum standard deviation.
+    """
+    super().__init__(name='MultivariateNormalDiagHead')
+    self._min_scale = min_scale
+    self._loc_layer = hk.Linear(num_dimensions)
+    self._scale_layer = hk.Linear(num_dimensions)
+
+  def __call__(self, inputs: jnp.ndarray) -> tfd.Distribution:
+    loc = self._loc_layer(inputs)
+    scale = self._scale_layer(inputs)
+    scale = jax.nn.softplus(scale) + self._min_scale
+    return tfd.MultivariateNormalDiag(loc=loc, scale_diag=scale)
