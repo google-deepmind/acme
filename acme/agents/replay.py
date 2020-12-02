7a3c8648bc61c8735fce11090f1f1206e86bc7ca
Ian Osband <iosband@google.com>
2020-12-02 05:16:35 -0800

    Factor R2D2 replay out of single-process agent.
    - No more reverb clutter in agent code.
    - Follows pattern of other Jax agents.
    
    PiperOrigin-RevId: 345212674
    Change-Id: I1ee80bd2fc7ebf771ff75a8322885982747fa367

--- acme/agents/replay.py
+++ acme/agents/replay.py
@@ -19,6 +19,7 @@ from typing import Iterator
 from acme import adders as adders_lib
 from acme import datasets
 from acme import specs
+from acme import types
 from acme.adders import reverb as adders
 import dataclasses
 import reverb
@@ -34,6 +35,7 @@ class ReverbReplay:
 
 def make_reverb_prioritized_nstep_replay(
     environment_spec: specs.EnvironmentSpec,
+    extra_spec: types.NestedSpec = (),
     n_step: int = 1,
     batch_size: int = 32,
     max_replay_size: int = 100_000,
@@ -43,17 +45,19 @@ def make_reverb_prioritized_nstep_replay(
     replay_table_name: str = adders.DEFAULT_PRIORITY_TABLE,
     prefetch_size: int = 4,
 ) -> ReverbReplay:
-  """Creates a single-process replay infrastructure from an environment spec."""
+  """Single-process replay for N-step TD data from an environment spec."""
   # Create a replay server to add data to. This uses no limiter behavior in
   # order to allow the Agent interface to handle it.
+  signature = adders.NStepTransitionAdder.signature(
+      environment_spec, extra_spec)
   replay_table = reverb.Table(
       name=replay_table_name,
       sampler=reverb.selectors.Prioritized(priority_exponent),
       remover=reverb.selectors.Fifo(),
       max_size=max_replay_size,
       rate_limiter=reverb.rate_limiters.MinSize(min_replay_size),
-      signature=adders.NStepTransitionAdder.signature(
-          environment_spec=environment_spec))
+      signature=signature,
+  )
   server = reverb.Server([replay_table], port=None)
 
   # The adder is used to insert observations into replay.
@@ -70,5 +74,55 @@ def make_reverb_prioritized_nstep_replay(
       prefetch_size=prefetch_size,
       environment_spec=environment_spec,
       transition_adder=True,
+  ).as_numpy_iterator()
+  return ReverbReplay(server, adder, data_iterator, client)
+
+
+def make_reverb_prioritized_sequence_replay(
+    environment_spec: specs.EnvironmentSpec,
+    extra_spec: types.NestedSpec = (),
+    batch_size: int = 32,
+    max_replay_size: int = 100_000,
+    min_replay_size: int = 1,
+    priority_exponent: float = 0.,
+    burn_in_length: int = 40,
+    sequence_length: int = 80,
+    sequence_period: int = 40,
+    replay_table_name: str = adders.DEFAULT_PRIORITY_TABLE,
+    prefetch_size: int = 4,
+) -> ReverbReplay:
+  """Single-process replay for sequence data from an environment spec."""
+  # Create a replay server to add data to. This uses no limiter behavior in
+  # order to allow the Agent interface to handle it.
+  replay_table = reverb.Table(
+      name=replay_table_name,
+      sampler=reverb.selectors.Prioritized(priority_exponent),
+      remover=reverb.selectors.Fifo(),
+      max_size=max_replay_size,
+      rate_limiter=reverb.rate_limiters.MinSize(min_replay_size),
+      signature=adders.SequenceAdder.signature(environment_spec, extra_spec),
+  )
+  server = reverb.Server([replay_table], port=None)
+
+  # The adder is used to insert observations into replay.
+  address = f'localhost:{server.port}'
+  client = reverb.Client(address)
+  sequence_length = burn_in_length + sequence_length + 1
+  adder = adders.SequenceAdder(
+      client=client,
+      period=sequence_period,
+      sequence_length=sequence_length,
+      delta_encoded=True,
+  )
+
+  # The dataset provides an interface to sample from replay.
+  data_iterator = datasets.make_reverb_dataset(
+      table=replay_table_name,
+      server_address=address,
+      batch_size=batch_size,
+      prefetch_size=prefetch_size,
+      environment_spec=environment_spec,
+      extra_spec=extra_spec,
+      sequence_length=sequence_length,
   ).as_numpy_iterator()
   return ReverbReplay(server, adder, data_iterator, client)
