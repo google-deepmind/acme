d3b053a7d4b3b041f204f94a8b300b48dc3903ee
Acme Contributor <noreply@google.com>
2020-12-02 06:00:43 -0800

    Implement GaussianMixtures in JAX.
    PiperOrigin-RevId: 345217480
    Change-Id: I3be7a130aa06e08f15d6fedfdce8f09309fb80f9

--- acme/jax/networks/distributional.py
+++ acme/jax/networks/distributional.py
@@ -24,6 +24,8 @@ import tensorflow_probability
 hk_init = hk.initializers
 tfd = tensorflow_probability.experimental.substrates.jax.distributions
 
+_MIN_SCALE = 1e-4
+
 
 class CategoricalHead(hk.Module):
   """Module that produces a categorical distribution with the given number of values."""
@@ -39,6 +41,103 @@ class CategoricalHead(hk.Module):
   def __call__(self, inputs: jnp.ndarray) -> tfd.Distribution:
     logits = self._linear(inputs)
     return tfd.Categorical(logits=logits)
+
+
+class GaussianMixture(hk.Module):
+  """Module that outputs a Gaussian Mixture Distribution."""
+
+  def __init__(self,
+               num_dimensions: int,
+               num_components: int,
+               multivariate: bool,
+               init_scale: Optional[float] = None,
+               name: str = 'GaussianMixture'):
+    """Initialization.
+
+    Args:
+      num_dimensions: dimensionality of the output distribution
+      num_components: number of mixture components.
+      multivariate: whether the resulting distribution is multivariate or not.
+      init_scale: the initial scale for the Gaussian mixture components.
+      name: name of the module passed to snt.Module parent class.
+    """
+    super().__init__(name=name)
+
+    self._num_dimensions = num_dimensions
+    self._num_components = num_components
+    self._multivariate = multivariate
+
+    if init_scale is not None:
+      self._scale_factor = init_scale / jax.nn.softplus(0.)
+    else:
+      self._scale_factor = 1.0  # Corresponds to init_scale = softplus(0).
+
+  def __call__(self,
+               inputs: jnp.ndarray,
+               low_noise_policy: bool = False) -> tfd.Distribution:
+    """Run the networks through inputs.
+
+    Args:
+      inputs: hidden activations of the policy network body.
+      low_noise_policy: whether to set vanishingly small scales for each
+        component. If this flag is set to True, the policy is effectively run
+        without Gaussian noise.
+
+    Returns:
+      Mixture Gaussian distribution.
+    """
+
+    # Define the weight initializer.
+    w_init = hk.initializers.VarianceScaling(scale=1e-5)
+
+    # Create a layer that outputs the unnormalized log-weights.
+    if self._multivariate:
+      logits_size = self._num_components
+    else:
+      logits_size = self._num_dimensions * self._num_components
+    logit_layer = hk.Linear(logits_size, w_init=w_init)
+
+    # Create two layers that outputs a location and a scale, respectively, for
+    # each dimension and each component.
+    loc_layer = hk.Linear(
+        self._num_dimensions * self._num_components, w_init=w_init)
+    scale_layer = hk.Linear(
+        self._num_dimensions * self._num_components, w_init=w_init)
+
+    # Compute logits, locs, and scales if necessary.
+    logits = logit_layer(inputs)
+    locs = loc_layer(inputs)
+
+    # When a low_noise_policy is requested, set the scales to its minimum value.
+    if low_noise_policy:
+      scales = jnp.full(locs.shape, _MIN_SCALE)
+    else:
+      scales = scale_layer(inputs)
+      scales = self._scale_factor * jax.nn.softplus(scales) + _MIN_SCALE
+
+    if self._multivariate:
+      components_class = tfd.MultivariateNormalDiag
+      shape = [-1, self._num_components, self._num_dimensions]
+      # In this case, no need to reshape logits as they are in the correct shape
+      # already, namely [batch_size, num_components].
+    else:
+      components_class = tfd.Normal
+      shape = [-1, self._num_dimensions, self._num_components]
+      logits = logits.reshape(shape)
+
+    # Reshape the mixture's location and scale parameters appropriately.
+    locs = locs.reshape(shape)
+    scales = scales.reshape(shape)
+
+    # Create the mixture distribution.
+    distribution = tfd.MixtureSameFamily(
+        mixture_distribution=tfd.Categorical(logits=logits),
+        components_distribution=components_class(loc=locs, scale=scales))
+
+    if not self._multivariate:
+      distribution = tfd.Independent(distribution)
+
+    return distribution
 
 
 class MultivariateNormalDiagHead(hk.Module):
