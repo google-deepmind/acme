8110a890137ea5c4cff4a23cfdc6235bdb89191b
Aref Moradi <rfmrd619@gmail.com>
2021-02-20 18:54:00 +0100

    Change bsuite dependency to point to PyPI package instead of Github repository

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ jax_requirements = [
 ]
 
 env_requirements = [
-    'bsuite @ git+git://github.com/deepmind/bsuite.git#egg=bsuite',
+    'bsuite',
     'dm-control',
     'gym',
     'gym[atari]',
