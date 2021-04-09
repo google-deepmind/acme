d34b15a50f0d2e5d9fb1da66cea19847ec56f160
Piotr Stanczyk <stanczyk@google.com>
2021-04-08 23:01:13 -0700

    Extract common functions out of the run_d4pg into helpers library.
    PiperOrigin-RevId: 367574916
    Change-Id: I3f7899b3ae1d31e24281fb8cd70f7e43e0675954

--- examples/control/run_d4pg.py
+++ examples/control/run_d4pg.py
@@ -15,21 +15,13 @@
 
 """Example running D4PG on the control suite."""
 
-from typing import Mapping, Sequence
-
 from absl import app
 from absl import flags
 import acme
 from acme import specs
-from acme import types
-from acme import wrappers
 from acme.agents.tf import actors
 from acme.agents.tf import d4pg
-from acme.tf import networks
-from acme.tf import utils as tf2_utils
-from dm_control import suite
-import dm_env
-import numpy as np
+from acme.examples.control import helpers
 import sonnet as snt
 
 FLAGS = flags.FLAGS
@@ -41,57 +33,11 @@ flags.DEFINE_integer('num_episodes_per_eval', 10,
                      'episodes.')
 
 
-def make_environment(domain_name: str = 'cartpole',
-                     task_name: str = 'balance') -> dm_env.Environment:
-  """Creates a control suite environment."""
-  environment = suite.load(domain_name, task_name)
-  environment = wrappers.SinglePrecisionWrapper(environment)
-  return environment
-
-
-def make_networks(
-    action_spec: specs.BoundedArray,
-    policy_layer_sizes: Sequence[int] = (256, 256, 256),
-    critic_layer_sizes: Sequence[int] = (512, 512, 256),
-    vmin: float = -150.,
-    vmax: float = 150.,
-    num_atoms: int = 51,
-) -> Mapping[str, types.TensorTransformation]:
-  """Creates networks used by the agent."""
-
-  # Get total number of action dimensions from action spec.
-  num_dimensions = np.prod(action_spec.shape, dtype=int)
-
-  # Create the shared observation network; here simply a state-less operation.
-  observation_network = tf2_utils.batch_concat
-
-  # Create the policy network.
-  policy_network = snt.Sequential([
-      networks.LayerNormMLP(policy_layer_sizes, activate_final=True),
-      networks.NearZeroInitializedLinear(num_dimensions),
-      networks.TanhToSpec(action_spec),
-  ])
-
-  # Create the critic network.
-  critic_network = snt.Sequential([
-      # The multiplexer concatenates the observations/actions.
-      networks.CriticMultiplexer(),
-      networks.LayerNormMLP(critic_layer_sizes, activate_final=True),
-      networks.DiscreteValuedHead(vmin, vmax, num_atoms),
-  ])
-
-  return {
-      'policy': policy_network,
-      'critic': critic_network,
-      'observation': observation_network,
-  }
-
-
 def main(_):
   # Create an environment, grab the spec, and use it to create networks.
-  environment = make_environment()
+  environment = helpers.make_environment()
   environment_spec = specs.make_environment_spec(environment)
-  agent_networks = make_networks(environment_spec.actions)
+  agent_networks = helpers.make_networks(environment_spec.actions)
 
   # Construct the agent.
   agent = d4pg.D4PG(
@@ -112,7 +58,7 @@ def main(_):
 
   # Create the evaluation actor and loop.
   eval_actor = actors.FeedForwardActor(policy_network=eval_policy)
-  eval_env = make_environment()
+  eval_env = helpers.make_environment()
   eval_loop = acme.EnvironmentLoop(eval_env, eval_actor, label='eval_loop')
 
   for _ in range(FLAGS.num_episodes // FLAGS.num_episodes_per_eval):
