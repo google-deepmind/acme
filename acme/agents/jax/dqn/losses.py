c602d16cc895b58fd11ab4f79867bb9f42314310
Nino Vieillard <vieillard@google.com>
2021-04-14 05:24:04 -0700

    Add losses for dqn-based agents.
    PiperOrigin-RevId: 368410028
    Change-Id: I3ce419657adfff684bbad7d6f2140b77dba83922

--- acme/agents/jax/dqn/losses.py
+++ acme/agents/jax/dqn/losses.py
@@ -74,3 +74,103 @@ class PrioritizedDoubleQLearning(learning_lib.LossFn):
         keys=keys, priorities=jnp.abs(td_error).astype(jnp.float64))
     extra = learning_lib.LossExtra(metrics={}, reverb_update=reverb_update)
     return loss, extra
+
+
+@dataclasses.dataclass
+class QLearning(learning_lib.LossFn):
+  """Clipped q learning.
+
+  This matches the original DQN loss: https://arxiv.org/abs/1312.5602.
+  """
+  discount: float = 0.99
+  max_abs_reward: float = 1.
+  huber_loss_parameter: float = 1.
+
+  def __call__(
+      self,
+      network: networks_lib.FeedForwardNetwork,
+      params: networks_lib.Params,
+      target_params: networks_lib.Params,
+      batch: reverb.ReplaySample,
+      key: networks_lib.PRNGKey,
+  ) -> Tuple[jnp.DeviceArray, learning_lib.LossExtra]:
+    """Calculate a loss on a single batch of data."""
+    del key
+    transitions: types.Transition = batch.data
+
+    # Forward pass.
+    q_tm1 = network.apply(params, transitions.observation)
+    q_t = network.apply(target_params, transitions.next_observation)
+
+    # Cast and clip rewards.
+    d_t = (transitions.discount * self.discount).astype(jnp.float32)
+    r_t = jnp.clip(transitions.reward, -self.max_abs_reward,
+                   self.max_abs_reward).astype(jnp.float32)
+
+    # Compute Q-learning TD-error.
+    batch_error = jax.vmap(rlax.q_learning)
+    td_error = batch_error(q_tm1, transitions.action, r_t, d_t, q_t)
+    batch_loss = rlax.huber_loss(td_error, self.huber_loss_parameter)
+
+    loss = jnp.mean(batch_loss)
+    extra = learning_lib.LossExtra(metrics={})
+    return loss, extra
+
+
+@dataclasses.dataclass
+class MunchausenQLearning(learning_lib.LossFn):
+  """Munchausen q learning.
+
+  Implements M-DQN: https://arxiv.org/abs/2007.14430.
+  """
+  entropy_temperature: float = 0.03  # tau parameter
+  munchausen_coefficient: float = 0.9  # alpha parameter
+  clip_value_min: float = -1e3
+  discount: float = 0.99
+  max_abs_reward: float = 1.
+  huber_loss_parameter: float = 1.
+
+  def __call__(
+      self,
+      network: networks_lib.FeedForwardNetwork,
+      params: networks_lib.Params,
+      target_params: networks_lib.Params,
+      batch: reverb.ReplaySample,
+      key: networks_lib.PRNGKey,
+  ) -> Tuple[jnp.DeviceArray, learning_lib.LossExtra]:
+    """Calculate a loss on a single batch of data."""
+    del key
+    transitions: types.Transition = batch.data
+
+    # Forward pass.
+    q_online_s = network.apply(params, transitions.observation)
+    action_one_hot = jax.nn.one_hot(transitions.action, q_online_s.shape[-1])
+    q_online_sa = jnp.sum(action_one_hot * q_online_s, axis=-1)
+    q_target_s = network.apply(target_params, transitions.observation)
+    q_target_next = network.apply(target_params, transitions.next_observation)
+
+    # Cast and clip rewards.
+    d_t = (transitions.discount * self.discount).astype(jnp.float32)
+    r_t = jnp.clip(transitions.reward, -self.max_abs_reward,
+                   self.max_abs_reward).astype(jnp.float32)
+
+    # Munchausen term : tau * log_pi(a|s)
+    munchausen_term = self.entropy_temperature * jax.nn.log_softmax(
+        q_target_s / self.entropy_temperature, axis=-1)
+    munchausen_term_a = jnp.sum(action_one_hot * munchausen_term, axis=-1)
+    munchausen_term_a = jnp.clip(munchausen_term_a,
+                                 a_min=self.clip_value_min,
+                                 a_max=0.)
+
+    # Soft Bellman operator applied to q
+    next_v = self.entropy_temperature * jax.nn.logsumexp(
+        q_target_next / self.entropy_temperature, axis=-1)
+    target_q = jax.lax.stop_gradient(r_t + self.munchausen_coefficient *
+                                     munchausen_term_a + d_t * next_v)
+
+    batch_loss = rlax.huber_loss(target_q - q_online_sa,
+                                 self.huber_loss_parameter)
+    loss = jnp.mean(batch_loss)
+
+    extra = learning_lib.LossExtra(metrics={})
+    return loss, extra
