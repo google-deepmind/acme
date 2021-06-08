4d18c54fbd65c3cdaa4a511c482ed9a20f7e8172
Piotr Stanczyk <stanczyk@google.com>
2021-06-08 06:47:10 -0700

    Do not import video wrappers in Acme codebase by default to avoid importing matplotlib by default (which is time-consuming).
    PiperOrigin-RevId: 378142592
    Change-Id: Iec91cb0ffda7ee4b2d4a32df022bd75f3ad4434d

--- acme/wrappers/__init__.py
+++ acme/wrappers/__init__.py
@@ -25,8 +25,6 @@ from acme.wrappers.gym_wrapper import GymWrapper
 from acme.wrappers.observation_action_reward import ObservationActionRewardWrapper
 from acme.wrappers.single_precision import SinglePrecisionWrapper
 from acme.wrappers.step_limit import StepLimitWrapper
-from acme.wrappers.video import MujocoVideoWrapper
-from acme.wrappers.video import VideoWrapper
 
 try:
   # pylint: disable=g-import-not-at-top
