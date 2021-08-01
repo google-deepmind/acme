0974b3408417213a08693ea4f1987b0523d2c8a6
Khush Jammu <khushjammu@gmail.com>
2021-08-01 15:39:58 +0800

    changes

--- acme/stonks.py
+++ acme/stonks.py
@@ -64,20 +64,20 @@ reverb_replay = replay.make_reverb_prioritized_nstep_replay(
 
 ### NETWORK
 
-# def network(x):
-#   model = hk.Sequential([
-#       hk.Flatten(),
-#       hk.nets.MLP([50, 50, spec.actions.num_values])
-#   ])
-#   return model(x)
+def network(x):
+  model = hk.Sequential([
+      hk.Flatten(),
+      hk.nets.MLP([50, 50, spec.actions.num_values])
+  ])
+  return model(x)
 
-# # Make network purely functional
-# network_hk = hk.without_apply_rng(hk.transform(network, apply_rng=True))
-# dummy_obs = utils.add_batch_dim(utils.zeros_like(spec.observations))
+# Make network purely functional
+network_hk = hk.without_apply_rng(hk.transform(network, apply_rng=True))
+dummy_obs = utils.add_batch_dim(utils.zeros_like(spec.observations))
 
-# network = networks_lib.FeedForwardNetwork(
-#   init=lambda rng: network_hk.init(rng, dummy_obs),
-#   apply=network_hk.apply)
+network = networks_lib.FeedForwardNetwork(
+  init=lambda rng: network_hk.init(rng, dummy_obs),
+  apply=network_hk.apply)
 
 
 
@@ -97,23 +97,6 @@ reverb_replay = replay.make_reverb_prioritized_nstep_replay(
 #     return int(1 / observations_per_step)
 
 
-# optimizer = optax.chain(
-#     optax.clip_by_global_norm(config.max_gradient_norm),
-#     optax.adam(config.learning_rate),
-# )
-
-# learner = learning.DQNLearner(
-#   network=network,# let's try having the same network
-#   random_key=key_learner,
-#   optimizer=optimizer,
-#   discount=config.discount,
-#   importance_sampling_exponent=config.importance_sampling_exponent,
-#   target_update_period=config.target_update_period,
-#   iterator=reverb_replay.data_iterator,
-#   replay_client=reverb_replay.client
-# )
-
-
 
 ### ACTOR: CREATE AND START ###
 
@@ -123,82 +106,118 @@ reverb_replay = replay.make_reverb_prioritized_nstep_replay(
 # this is to start populating the replay buffer, aka self-play
 
 # @ray.remote
-class StonksActor():
+# class StonksActor():
+#   def __init__(self, config, address):
+#     key_learner, key_actor = jax.random.split(jax.random.PRNGKey(config.seed))
+
+#     client = reverb.Client(address)
+#     adder = adders.NStepTransitionAdder(client, config.n_step, config.discount)
+
+
+#     def network(x):
+#       model = hk.Sequential([
+#           hk.Flatten(),
+#           hk.nets.MLP([50, 50, spec.actions.num_values])
+#       ])
+#       return model(x)
+
+#     # Make network purely functional
+#     network_hk = hk.without_apply_rng(hk.transform(network, apply_rng=True))
+#     dummy_obs = utils.add_batch_dim(utils.zeros_like(spec.observations))
+
+#     network = networks_lib.FeedForwardNetwork(
+#       init=lambda rng: network_hk.init(rng, dummy_obs),
+#       apply=network_hk.apply)
+
+
+#     def policy(params: networks_lib.Params, key: jnp.ndarray,
+#                observation: jnp.ndarray) -> jnp.ndarray:
+#       action_values = network.apply(params, observation) # how will this work when they're on different devices?
+#       return rlax.epsilon_greedy(config.epsilon).sample(key, action_values)
+
+#     actor = actors.FeedForwardActor(
+#       policy=policy,
+#       random_key=key_actor,
+#       variable_client=variable_utils.VariableClient(learner, ''), # need to write a custom wrapper around learner so it calls .remote
+#       adder=adder)
+
+#     self.loop = acme.EnvironmentLoop(environment, actor, should_update=True)
+#     # print("running environment loop")
+#     # loop.run(num_steps=NUM_STEPS_ACTOR) # we'll probably want a custom environment loop which reads a sharedstorage to decide whether to terminate
+
+#   def run(self):
+#     self.loop.run(num_steps=10)
+
+
+
+class StonksLearner():
   def __init__(self, config, address):
-    key_learner, key_actor = jax.random.split(jax.random.PRNGKey(config.seed))
+    self.config = config
 
-    client = reverb.Client(address)
-    adder = adders.NStepTransitionAdder(client, config.n_step, config.discount)
+    optimizer = optax.chain(
+        optax.clip_by_global_norm(config.max_gradient_norm),
+        optax.adam(config.learning_rate),
+    )
 
+    self.learner = learning.DQNLearner(
+      network=network,# let's try having the same network
+      random_key=key_learner,
+      optimizer=optimizer,
+      discount=config.discount,
+      importance_sampling_exponent=config.importance_sampling_exponent,
+      target_update_period=config.target_update_period,
+      iterator=reverb_replay.data_iterator,
+      replay_client=reverb_replay.client
+    )
 
-    def network(x):
-      model = hk.Sequential([
-          hk.Flatten(),
-          hk.nets.MLP([50, 50, spec.actions.num_values])
-      ])
-      return model(x)
+    self.client = reverb.Client(address)
 
-    # Make network purely functional
-    network_hk = hk.without_apply_rng(hk.transform(network, apply_rng=True))
-    dummy_obs = utils.add_batch_dim(utils.zeros_like(spec.observations))
-
-    network = networks_lib.FeedForwardNetwork(
-      init=lambda rng: network_hk.init(rng, dummy_obs),
-      apply=network_hk.apply)
-
-
-    def policy(params: networks_lib.Params, key: jnp.ndarray,
-               observation: jnp.ndarray) -> jnp.ndarray:
-      action_values = network.apply(params, observation) # how will this work when they're on different devices?
-      return rlax.epsilon_greedy(config.epsilon).sample(key, action_values)
-
-    actor = actors.FeedForwardActor(
-      policy=policy,
-      random_key=key_actor,
-      variable_client=variable_utils.VariableClient(learner, ''), # need to write a custom wrapper around learner so it calls .remote
-      adder=adder)
-
-    self.loop = acme.EnvironmentLoop(environment, actor, should_update=True)
-    # print("running environment loop")
-    # loop.run(num_steps=NUM_STEPS_ACTOR) # we'll probably want a custom environment loop which reads a sharedstorage to decide whether to terminate
+  @staticmethod
+  def _calculate_num_learner_steps(num_observations: int, min_observations: int, observations_per_step: float) -> int:
+    """Calculates the number of learner steps to do at step=num_observations."""
+    n = num_observations - min_observations
+    if n < 0:
+      # Do not do any learner steps until you have seen min_observations.
+      return 0
+    if observations_per_step > 1:
+      # One batch every 1/obs_per_step observations, otherwise zero.
+      return int(n % int(observations_per_step) == 0)
+    else:
+      # Always return 1/obs_per_step batches every observation.
+      return int(1 / observations_per_step)
 
   def run(self):
-    self.loop.run(num_steps=10)
+    # we just keep count of the number of steps it's trained on
+    step_count = 0
 
+    while step_count < 10:
+      num_transitions = self.client.server_info()['priority_table'].num_episodes # should be ok?
 
+      if num_episodes < MIN_OBSERVATIONS:
+        print("sleeping")
+        sleep(1)
+        continue
 
-def run_actor():
-  pass
+      num_steps = self._calculate_num_learner_steps(
+        num_observations=num_transitions,
+        min_observations=MIN_OBSERVATIONS,
+        observations_per_step=self.config.batch_size / self.config.samples_per_insert,
+        )
 
+      for _ in range(num_steps):
+        self.learner.step()
 
-# @ray.remote
-def run_learner():
-  client = reverb.Client(address)
-
-  # we just keep count of the number of steps it's trained on
-  step_count = 0
-
-  while not should_terminate(episode_count, step_count):
-    num_transitions = client.server_info()['priority_table'].num_episodes # should be ok?
-
-    if num_episodes < MIN_OBSERVATIONS:
-      sleep(0.5)
-      continue
-
-    num_steps = _calculate_num_learner_steps(
-      num_observations=num_transitions,
-      min_observations=MIN_OBSERVATIONS,
-      observations_per_step=config.batch_size / config.samples_per_insert,
-      )
-
-    for _ in range(num_steps):
-      learner.step()
+      step_count += num_steps
 
 if __name__ == "__main__":
   ray.init()
 
-  a = StonksActor(config, reverb_replay.address)
-  a.run()
+  learner = StonksLearner.remote(config, reverb_replay.address)
+
+  ray.get(learner.run.remote())
+
+  # a = StonksActor(config, reverb_replay.address)
+  # a.run()
   # a = StonksActor.remote(config)
   # ray.get(a.run.remote())
   # run_actor.remote()
