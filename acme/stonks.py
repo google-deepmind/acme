3f5c11e59cff68f9d8534f7e5d62b496f5deb112
Khush Jammu <khushjammu@gmail.com>
2021-08-01 15:00:01 +0800

    changes

--- acme/stonks.py
+++ acme/stonks.py
@@ -33,7 +33,7 @@ config = dqn_config.DQNConfig(
 MIN_OBSERVATIONS = max(config.batch_size, config.min_replay_size)
 NUM_STEPS_ACTOR = 20 # taken from agent_test
 
-key_learner, key_actor = jax.random.split(jax.random.PRNGKey(config.seed))
+# key_learner, key_actor = jax.random.split(jax.random.PRNGKey(config.seed))
 
 
 
@@ -123,6 +123,30 @@ reverb_replay = replay.make_reverb_prioritized_nstep_replay(
 @ray.remote
 class StonksActor():
   def __init__(self):
+    import acme
+    from acme import specs
+    from acme.jax import utils
+    from acme.jax import variable_utils
+    from acme.jax import networks as networks_lib
+    from acme.agents import replay
+    from acme.agents.jax import actors
+    from acme.agents.jax.dqn import learning
+    from acme.agents.jax.dqn import config as dqn_config
+    from acme.testing import fakes
+
+    from acme.adders import reverb as adders
+
+    import ray
+    import jax
+    import jax.numpy as jnp
+    import rlax
+    import optax
+    import reverb
+    import numpy as np
+    import haiku as hk
+
+    key_learner, key_actor = jax.random.split(jax.random.PRNGKey(config.seed))
+
     address = 'localhost:8000'
     client = reverb.Client(address)
     adder = adders.NStepTransitionAdder(client, config.n_step, config.discount)
