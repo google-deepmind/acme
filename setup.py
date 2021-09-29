44c34d1a09b1cc79f2c7452097846c1fdfc8d608
Anurag Kumar <mailanu98@gmail.com>
2021-09-30 00:33:38 +0530

    Update setup.py
    updated classifiers

--- setup.py
+++ setup.py
@@ -119,6 +119,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Scientific/Engineering :: Artificial Intelligence',
     ],
 )
