a57ac21850ef4cbd496d0a6b5f85ddff97341a2a
Acme Contributor <noreply@google.com>
2022-04-01 10:47:34 -0700

    Adds iterator utilities.
    PiperOrigin-RevId: 438859780
    Change-Id: I3d277d21b1a23cbd386da064783fbb49df60c68c

---
+++ acme/utils/iterator_utils.py
@@ -0,0 +1,39 @@
+# python3
+# Copyright 2018 DeepMind Technologies Limited. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Iterator utilities."""
+import itertools
+import operator
+from typing import Any, Iterator, List, Sequence
+
+
+def unzip_iterators(zipped_iterators: Iterator[Sequence[Any]],
+                    num_sub_iterators: int) -> List[Iterator[Any]]:
+  """Returns unzipped iterators.
+
+  Note that simply returning:
+    [(x[i] for x in iter_tuple[i]) for i in range(num_sub_iterators)]
+  seems to cause all iterators to point to the final value of i, thus causing
+  all sub_learners to consume data from this final iterator.
+
+  Args:
+    zipped_iterators: zipped iterators (e.g., from zip_iterators()).
+    num_sub_iterators: the number of sub-iterators in the zipped iterator.
+  """
+  iter_tuple = itertools.tee(zipped_iterators, num_sub_iterators)
+  return [
+      map(operator.itemgetter(i), iter_tuple[i])
+      for i in range(num_sub_iterators)
+  ]
---
+++ acme/utils/iterator_utils_test.py
@@ -0,0 +1,39 @@
+# Copyright 2018 DeepMind Technologies Limited. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Tests for iterator_utils."""
+
+from absl.testing import absltest
+from acme.utils import iterator_utils
+import numpy as np
+
+
+class IteratorUtilsTest(absltest.TestCase):
+
+  def test_iterator_zipping(self):
+
+    def get_iters():
+      x = iter(range(0, 10))
+      y = iter(range(20, 30))
+      return [x, y]
+
+    zipped = zip(*get_iters())
+    unzipped = iterator_utils.unzip_iterators(zipped, num_sub_iterators=2)
+    expected_x, expected_y = get_iters()
+    np.testing.assert_equal(list(unzipped[0]), list(expected_x))
+    np.testing.assert_equal(list(unzipped[1]), list(expected_y))
+
+
+if __name__ == '__main__':
+  absltest.main()
