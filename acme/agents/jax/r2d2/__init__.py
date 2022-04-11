52a5f758d5580b3e9effaa7506ba49274a772a7f
Danila Sinopalnikov <sinopalnikov@google.com>
2022-04-11 13:52:38 -0700

    Internal change
    PiperOrigin-RevId: 440987525
    Change-Id: Ib93b54fa9be70b530db0393f664750b9ad07958b

--- acme/agents/jax/r2d2/__init__.py
+++ acme/agents/jax/r2d2/__init__.py
@@ -21,6 +21,7 @@ from acme.agents.jax.r2d2.agents import R2D2
 from acme.agents.jax.r2d2.builder import R2D2Builder
 from acme.agents.jax.r2d2.config import R2D2Config
 from acme.agents.jax.r2d2.learning import R2D2Learner
+from acme.agents.jax.r2d2.networks import EpsilonRecurrentPolicy
 from acme.agents.jax.r2d2.networks import make_atari_networks
 from acme.agents.jax.r2d2.networks import make_behavior_policy
 from acme.agents.jax.r2d2.networks import make_networks
