fc4a9bfeb7705257a74916cc3c7e49522473c2a1
Piotr Stanczyk <stanczyk@google.com>
2022-04-28 09:27:48 -0700

    Update Acme to the new Reverb+Launchpad release.
    PiperOrigin-RevId: 445172000
    Change-Id: Ia05a8d4ad75c97132c0561ea3483618ffb9b7e32

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ spec.loader.exec_module(_metadata)
 # sure this constraint is upheld.
 
 tensorflow = [
-    'dm-reverb==0.7.1',
+    'dm-reverb==0.7.2',
     'keras==2.8.0',
     'tensorflow-datasets==4.5.2',
     'tensorflow-estimator==2.8.0',
@@ -49,7 +49,7 @@ tensorflow = [
 core_requirements = [
     'absl-py',
     'dm-env',
-    'dm-launchpad==0.5.1',
+    'dm-launchpad==0.5.2',
     'dm-tree',
     'numpy',
     'pillow',
