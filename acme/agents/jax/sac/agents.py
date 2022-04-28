6fe8bbb6bebeaa5d84304c30ccb9264a9b67901a
Piotr Stanczyk <stanczyk@google.com>
2022-04-28 09:28:03 -0700

    Turn DistributedSAC into a function.
    PiperOrigin-RevId: 445172058
    Change-Id: I95581eb6cadfbf06c3470786959d8c0fd8eaa05c

--- acme/agents/jax/sac/agents.py
+++ acme/agents/jax/sac/agents.py
@@ -28,73 +28,89 @@ from acme.jax.layouts import distributed_layout
 from acme.jax.layouts import local_layout
 from acme.utils import counting
 from acme.utils import loggers
+import launchpad as lp
 
 NetworkFactory = Callable[[specs.EnvironmentSpec], networks.SACNetworks]
 
 
+# TODO(stanczyk): Remove DistributedTD3 once not used.
 class DistributedSAC(distributed_layout.DistributedLayout):
-  """Distributed program definition for SAC."""
+  """Distributed program definition for SAC.
 
-  def __init__(
-      self,
-      environment_factory: jax_types.EnvironmentFactory,
-      network_factory: NetworkFactory,
-      config: sac_config.SACConfig,
-      seed: int,
-      num_actors: int,
-      max_number_of_steps: Optional[int] = None,
-      log_to_bigtable: bool = False,
-      log_every: float = 10.0,
-      normalize_input: bool = True,
-      evaluator_factories: Optional[Sequence[
-          distributed_layout.EvaluatorFactory]] = None,
-  ):
-    logger_fn = functools.partial(
-        loggers.make_default_logger,
-        'learner',
-        log_to_bigtable,
-        time_delta=log_every,
-        asynchronous=True,
-        serialize_fn=utils.fetch_devicearray,
-        steps_key='learner_steps')
-    sac_builder = builder.SACBuilder(config, logger_fn=logger_fn)
-    if normalize_input:
-      dummy_seed = 1
-      environment_spec = specs.make_environment_spec(
-          environment_factory(dummy_seed))
-      # One batch dimension: [batch_size, ...]
-      batch_dims = (0,)
-      sac_builder = normalization.NormalizationBuilder(
-          sac_builder,
-          environment_spec,
-          is_sequence_based=False,
-          batch_dims=batch_dims)
-    if evaluator_factories is None:
-      eval_policy_factory = (
-          lambda n: networks.apply_policy_and_sample(n, True))
-      evaluator_factories = [
-          distributed_layout.default_evaluator_factory(
-              environment_factory=environment_factory,
-              network_factory=network_factory,
-              policy_factory=eval_policy_factory,
-              log_to_bigtable=log_to_bigtable)
-      ]
-    super().__init__(
-        seed=seed,
-        environment_factory=environment_factory,
-        network_factory=network_factory,
-        builder=sac_builder,
-        policy_network=networks.apply_policy_and_sample,
-        evaluator_factories=evaluator_factories,
-        num_actors=num_actors,
-        max_number_of_steps=max_number_of_steps,
-        prefetch_size=config.prefetch_size,
-        log_to_bigtable=log_to_bigtable,
-        actor_logger_fn=distributed_layout.get_default_logger_fn(
-            log_to_bigtable, log_every),
-        checkpointing_config=distributed_layout.CheckpointingConfig(),
-        make_snapshot_models=networks.default_models_to_snapshot,
-    )
+    DEPRECATED: Use distributed_sac function instead.
+  """
+
+  def __init__(self, *args, **kwargs):
+    self.args = args
+    self.kwargs = kwargs
+
+  def build(self, name='agent', program: Optional[lp.Program] = None):
+    """Build the distributed agent topology."""
+    return make_distributed_sac(
+        *self.args, name=name, program=program, **self.kwargs)
+
+
+def make_distributed_sac(environment_factory: jax_types.EnvironmentFactory,
+                         network_factory: NetworkFactory,
+                         config: sac_config.SACConfig,
+                         seed: int,
+                         num_actors: int,
+                         max_number_of_steps: Optional[int] = None,
+                         log_to_bigtable: bool = False,
+                         log_every: float = 10.0,
+                         normalize_input: bool = True,
+                         evaluator_factories: Optional[Sequence[
+                             distributed_layout.EvaluatorFactory]] = None,
+                         name: str = 'agent',
+                         program: Optional[lp.Program] = None):
+  """Builds distributed SAC program."""
+  logger_fn = functools.partial(
+      loggers.make_default_logger,
+      'learner',
+      log_to_bigtable,
+      time_delta=log_every,
+      asynchronous=True,
+      serialize_fn=utils.fetch_devicearray,
+      steps_key='learner_steps')
+  sac_builder = builder.SACBuilder(config, logger_fn=logger_fn)
+  if normalize_input:
+    dummy_seed = 1
+    environment_spec = specs.make_environment_spec(
+        environment_factory(dummy_seed))
+    # One batch dimension: [batch_size, ...]
+    batch_dims = (0,)
+    sac_builder = normalization.NormalizationBuilder(
+        sac_builder,
+        environment_spec,
+        is_sequence_based=False,
+        batch_dims=batch_dims)
+  if evaluator_factories is None:
+    eval_policy_factory = (
+        lambda n: networks.apply_policy_and_sample(n, True))
+    evaluator_factories = [
+        distributed_layout.default_evaluator_factory(
+            environment_factory=environment_factory,
+            network_factory=network_factory,
+            policy_factory=eval_policy_factory,
+            log_to_bigtable=log_to_bigtable)
+    ]
+  return distributed_layout.make_distributed_program(
+      seed=seed,
+      environment_factory=environment_factory,
+      network_factory=network_factory,
+      builder=sac_builder,
+      policy_network_factory=networks.apply_policy_and_sample,
+      evaluator_factories=evaluator_factories,
+      num_actors=num_actors,
+      max_number_of_steps=max_number_of_steps,
+      prefetch_size=config.prefetch_size,
+      log_to_bigtable=log_to_bigtable,
+      actor_logger_fn=distributed_layout.get_default_logger_fn(
+          log_to_bigtable, log_every),
+      checkpointing_config=distributed_layout.CheckpointingConfig(),
+      make_snapshot_models=networks.default_models_to_snapshot,
+      name=name,
+      program=program)
 
 
 class SAC(local_layout.LocalLayout):
