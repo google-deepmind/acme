93247956ea4036d2210e92f9ff762f62463a4427
Piotr Stanczyk <stanczyk@google.com>
2022-04-28 09:28:14 -0700

    Turn DistributedD4PG into a function.
    PiperOrigin-RevId: 445172107
    Change-Id: I00f71ca3865c5652eca134f4ce3933547c2e1bf1

--- acme/agents/jax/d4pg/agents.py
+++ acme/agents/jax/d4pg/agents.py
@@ -27,94 +27,106 @@ from acme.jax.layouts import local_layout
 from acme.utils import counting
 from acme.utils import loggers
 import dm_env
+import launchpad as lp
 
 NetworkFactory = Callable[[specs.EnvironmentSpec], d4pg_networks.D4PGNetworks]
 
 
+# TODO(stanczyk): Remove DistributedD4PG once not used.
 class DistributedD4PG(distributed_layout.DistributedLayout):
-  """Program definition for D4PG.
+  """Distributed program definition for SAC.
 
-  We are in the process of migrating towards a more modular agent configuration.
-  This is maintained now for compatibility.
+    DEPRECATED: Use distributed_sac function instead.
   """
 
-  def __init__(
-      self,
-      environment_factory: Callable[[bool], dm_env.Environment],
-      network_factory: NetworkFactory,
-      random_seed: int,
-      num_actors: int,
-      environment_spec: Optional[specs.EnvironmentSpec] = None,
-      batch_size: int = 256,
-      prefetch_size: int = 2,
-      min_replay_size: int = 1000,
-      max_replay_size: int = 1000000,
-      samples_per_insert: float = 32.0,
-      n_step: int = 5,
-      sigma: float = 0.3,
-      clipping: bool = True,
-      discount: float = 0.99,
-      target_update_period: int = 100,
-      device_prefetch: bool = True,
-      log_to_bigtable: bool = False,
-      log_every: float = 10.0,
-      evaluator_factories: Optional[Sequence[
-          distributed_layout.EvaluatorFactory]] = None,
-  ):
-    config = d4pg_config.D4PGConfig(
-        discount=discount,
-        learning_rate=1e-4,
-        batch_size=batch_size,
-        prefetch_size=prefetch_size,
-        target_update_period=target_update_period,
-        min_replay_size=min_replay_size,
-        max_replay_size=max_replay_size,
-        samples_per_insert=samples_per_insert,
-        n_step=n_step,
-        sigma=sigma,
-        clipping=clipping,
-    )
-    logger_fn = functools.partial(
-        loggers.make_default_logger,
-        'learner',
-        log_to_bigtable,
-        time_delta=log_every,
-        asynchronous=True,
-        serialize_fn=utils.fetch_devicearray,
-        steps_key='learner_steps')
-    builder = d4pg_builder.D4PGBuilder(config, logger_fn=logger_fn)
+  def __init__(self, *args, **kwargs):
+    self.args = args
+    self.kwargs = kwargs
 
-    def _policy_network(networks):
-      return d4pg_networks.get_default_behavior_policy(networks, config=config)
+  def build(self, name='agent', program: Optional[lp.Program] = None):
+    """Build the distributed agent topology."""
+    return make_distributed_d4pg(
+        *self.args, name=name, program=program, **self.kwargs)
 
-    if evaluator_factories is None:
 
-      def _eval_policy_network(networks):
-        return d4pg_networks.get_default_eval_policy(networks)
+def make_distributed_d4pg(
+    environment_factory: Callable[[bool], dm_env.Environment],
+    network_factory: NetworkFactory,
+    random_seed: int,
+    num_actors: int,
+    environment_spec: Optional[specs.EnvironmentSpec] = None,
+    batch_size: int = 256,
+    prefetch_size: int = 2,
+    min_replay_size: int = 1000,
+    max_replay_size: int = 1000000,
+    samples_per_insert: float = 32.0,
+    n_step: int = 5,
+    sigma: float = 0.3,
+    clipping: bool = True,
+    discount: float = 0.99,
+    target_update_period: int = 100,
+    device_prefetch: bool = True,
+    log_to_bigtable: bool = False,
+    log_every: float = 10.0,
+    evaluator_factories: Optional[Sequence[
+        distributed_layout.EvaluatorFactory]] = None,
+    name: str = 'agent',
+    program: Optional[lp.Program] = None):
+  """Builds distributed D4PG program."""
+  config = d4pg_config.D4PGConfig(
+      discount=discount,
+      learning_rate=1e-4,
+      batch_size=batch_size,
+      prefetch_size=prefetch_size,
+      target_update_period=target_update_period,
+      min_replay_size=min_replay_size,
+      max_replay_size=max_replay_size,
+      samples_per_insert=samples_per_insert,
+      n_step=n_step,
+      sigma=sigma,
+      clipping=clipping,
+  )
+  logger_fn = functools.partial(
+      loggers.make_default_logger,
+      'learner',
+      log_to_bigtable,
+      time_delta=log_every,
+      asynchronous=True,
+      serialize_fn=utils.fetch_devicearray,
+      steps_key='learner_steps')
+  builder = d4pg_builder.D4PGBuilder(config, logger_fn=logger_fn)
 
-      evaluator_factories = [
-          distributed_layout.default_evaluator_factory(
-              environment_factory=lambda seed: environment_factory(True),
-              network_factory=network_factory,
-              policy_factory=_eval_policy_network,
-              log_to_bigtable=log_to_bigtable)
-      ]
+  def _policy_network(networks):
+    return d4pg_networks.get_default_behavior_policy(networks, config=config)
 
-    super().__init__(
-        seed=random_seed,
-        environment_factory=lambda seed: environment_factory(False),
-        network_factory=network_factory,
-        builder=builder,
-        policy_network=_policy_network,
-        evaluator_factories=evaluator_factories,
-        num_actors=num_actors,
-        environment_spec=environment_spec,
-        device_prefetch=device_prefetch,
-        log_to_bigtable=log_to_bigtable,
-        actor_logger_fn=distributed_layout.get_default_logger_fn(
-            log_to_bigtable, log_every),
-        prefetch_size=config.prefetch_size,
-    )
+  if evaluator_factories is None:
+
+    def _eval_policy_network(networks):
+      return d4pg_networks.get_default_eval_policy(networks)
+
+    evaluator_factories = [
+        distributed_layout.default_evaluator_factory(
+            environment_factory=lambda seed: environment_factory(True),
+            network_factory=network_factory,
+            policy_factory=_eval_policy_network,
+            log_to_bigtable=log_to_bigtable)
+    ]
+  return distributed_layout.make_distributed_program(
+      seed=random_seed,
+      environment_factory=lambda seed: environment_factory(False),
+      network_factory=network_factory,
+      builder=builder,
+      policy_network_factory=_policy_network,
+      evaluator_factories=evaluator_factories,
+      num_actors=num_actors,
+      environment_spec=environment_spec,
+      device_prefetch=device_prefetch,
+      log_to_bigtable=log_to_bigtable,
+      actor_logger_fn=distributed_layout.get_default_logger_fn(
+          log_to_bigtable, log_every),
+      prefetch_size=config.prefetch_size,
+      name=name,
+      program=program)
 
 
 class D4PG(local_layout.LocalLayout):
