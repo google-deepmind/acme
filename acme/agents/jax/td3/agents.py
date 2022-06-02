c6d1e1aa8355d4fbc976e605c3f412b3e1460711
Piotr Stanczyk <stanczyk@google.com>
2022-06-02 02:02:30 -0700

    Delete deprecated make_distributed_td3.
    PiperOrigin-RevId: 452486372
    Change-Id: Ie27d4ce0d320a68f8858c50c7b426d0ed0e423f4

--- acme/agents/jax/td3/agents.py
+++ acme/agents/jax/td3/agents.py
@@ -14,100 +14,16 @@
 
 """Defines distributed and local TD3 agents, using JAX."""
 
-import functools
-from typing import Callable, Optional, Sequence
+from typing import Callable, Optional
 
 from acme import specs
 from acme.agents.jax.td3 import builder
 from acme.agents.jax.td3 import config as td3_config
 from acme.agents.jax.td3 import networks
-from acme.jax import experiments
-from acme.jax import types as jax_types
-from acme.jax import utils
-from acme.jax.layouts import distributed_layout
 from acme.jax.layouts import local_layout
 from acme.utils import counting
-from acme.utils import loggers
-import launchpad as lp
 
 NetworkFactory = Callable[[specs.EnvironmentSpec], networks.TD3Networks]
-
-
-# TODO(stanczyk): Remove DistributedTD3 once not used.
-class DistributedTD3(distributed_layout.DistributedLayout):
-  """Distributed program definition for TD3.
-
-    DEPRECATED: Use distributed_td3 function instead.
-  """
-
-  def __init__(self, *args, **kwargs):
-    self.args = args
-    self.kwargs = kwargs
-
-  def build(self, name='agent', program: Optional[lp.Program] = None):
-    """Build the distributed agent topology."""
-    return make_distributed_td3(
-        *self.args, name=name, program=program, **self.kwargs)
-
-
-def make_distributed_td3(environment_factory: jax_types.EnvironmentFactory,
-                         environment_spec: specs.EnvironmentSpec,
-                         network_factory: NetworkFactory,
-                         config: td3_config.TD3Config,
-                         seed: int,
-                         num_actors: int,
-                         max_number_of_steps: Optional[int] = None,
-                         save_logs: bool = False,
-                         log_every: float = 10.0,
-                         evaluator_factories: Optional[Sequence[
-                             distributed_layout.EvaluatorFactory]] = None,
-                         name: str = 'agent',
-                         program: Optional[lp.Program] = None):
-  """Builds distributed TD3 program."""
-  logger_fn = functools.partial(
-      loggers.make_default_logger,
-      'learner',
-      save_logs,
-      time_delta=log_every,
-      asynchronous=True,
-      serialize_fn=utils.fetch_devicearray,
-      steps_key='learner_steps')
-  td3_builder = builder.TD3Builder(config)
-
-  action_specs = environment_spec.actions
-  policy_network_fn = functools.partial(
-      networks.get_default_behavior_policy,
-      action_specs=action_specs,
-      sigma=config.sigma)
-
-  if evaluator_factories is None:
-    eval_network_fn = functools.partial(
-        networks.get_default_behavior_policy,
-        action_specs=action_specs,
-        sigma=0.)
-    evaluator_factories = [
-        distributed_layout.default_evaluator_factory(
-            environment_factory=environment_factory,
-            network_factory=network_factory,
-            policy_factory=eval_network_fn,
-            save_logs=save_logs)
-    ]
-  experiment = experiments.Config(
-      builder=td3_builder,
-      environment_factory=environment_factory,
-      network_factory=network_factory,
-      policy_network_factory=policy_network_fn,
-      evaluator_factories=evaluator_factories,
-      seed=seed,
-      max_number_of_steps=max_number_of_steps,
-      logger_factory=distributed_layout.logger_factory(logger_fn, None,
-                                                       save_logs, log_every))
-  return experiments.make_distributed_experiment(
-      experiment=experiment,
-      num_actors=num_actors,
-      prefetch_size=config.prefetch_size,
-      name=name,
-      program=program)
 
 
 class TD3(local_layout.LocalLayout):
