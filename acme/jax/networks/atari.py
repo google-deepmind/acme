28f9e3d1d9274a71f69c2abc25c62982f4def5d5
Bobak Shahriari <bshahr@google.com>
2022-07-08 06:15:41 -0700

    Make R2D2 use the DeepAtariTorso by default.
    PiperOrigin-RevId: 459742581
    Change-Id: Iaebd2bd5ac22a5ce9d32c5859c7f6bc10591b1e4

--- acme/jax/networks/atari.py
+++ acme/jax/networks/atari.py
@@ -174,7 +174,7 @@ class R2D2AtariNetwork(hk.RNNCore):
 
   def __init__(self, num_actions: int):
     super().__init__(name='r2d2_atari_network')
-    self._embed = embedding.OAREmbedding(AtariTorso(), num_actions)
+    self._embed = embedding.OAREmbedding(DeepAtariTorso(), num_actions)
     self._core = hk.LSTM(512)
     self._duelling_head = duelling.DuellingMLP(num_actions, hidden_sizes=[512])
     self._num_actions = num_actions
