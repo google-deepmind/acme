9b04dfa992c66f06e7121246d3acebc2def18dd4
Danila Sinopalnikov <sinopalnikov@google.com>
2022-07-25 11:03:19 -0700

    Remove the deprecated agent classes for DQN. DQN is supposed to be used through the DQNBuilder + acme.jax.experiments interface.
    PiperOrigin-RevId: 463129181
    Change-Id: I8d5b2050234567cfb4a39aabaa5e1dc6abccc7e4

--- acme/agents/jax/dqn/__init__.py
+++ acme/agents/jax/dqn/__init__.py
@@ -18,7 +18,6 @@ from acme.agents.jax.dqn.actor import behavior_policy
 from acme.agents.jax.dqn.actor import default_behavior_policy
 from acme.agents.jax.dqn.actor import Epsilon
 from acme.agents.jax.dqn.actor import EpsilonPolicy
-from acme.agents.jax.dqn.agent import DQN
 from acme.agents.jax.dqn.builder import DQNBuilder
 from acme.agents.jax.dqn.config import DQNConfig
 from acme.agents.jax.dqn.learning import DQNLearner
