54ae175a9b18bc0f855b46c4f4099e5c02a288d8
Kamyar Ghasemipour <kamyar@google.com>
2022-08-10 22:33:32 -0700

    Bug fix.
    PiperOrigin-RevId: 466868296
    Change-Id: I25d1bf632fcfc4a83662da3d8d69ea9a7b20117b

--- acme/agents/jax/ppo/learning.py
+++ acme/agents/jax/ppo/learning.py
@@ -243,7 +243,7 @@ class PPOLearner(acme.Learner):
       # Update the EMA counter and obtain the zero debiasing multiplier
       if normalize_advantage or normalize_value:
         ema_counter = state.ema_counter + 1
-        state._replace(ema_counter=ema_counter)
+        state = state._replace(ema_counter=ema_counter)
         zero_debias = 1. / (1. - jnp.power(normalization_ema_tau, ema_counter))
 
       # Extract the data.
