d9c431996f3bb232640d412d719be77ca49a31ff
Peter Hawkins <phawkins@google.com>
2024-08-13 08:34:16 -0700

    [numpy] Fix test failures under NumPy 2.0.
    PiperOrigin-RevId: 662528924
    Change-Id: I0a1215f5c7d097cc37c3953a05cd4aebab4701a4

--- acme/wrappers/single_precision.py
+++ acme/wrappers/single_precision.py
@@ -75,11 +75,11 @@ def _convert_value(nested_value: types.Nest) -> types.Nest:
 
   def _convert_single_value(value):
     if value is not None:
-      value = np.array(value, copy=False)
+      value = np.asarray(value)
       if np.issubdtype(value.dtype, np.float64):
-        value = np.array(value, copy=False, dtype=np.float32)
+        value = np.asarray(value, dtype=np.float32)
       elif np.issubdtype(value.dtype, np.int64):
-        value = np.array(value, copy=False, dtype=np.int32)
+        value = np.asarray(value, dtype=np.int32)
     return value
 
   return tree.map_structure(_convert_single_value, nested_value)
